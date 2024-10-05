68a114e97ca197af652e52b93f64da0cd937e2b6
Jeffrey A <agyepong99@gmail.com>
2024-10-05 11:08:10 -0600

    first

---
+++ src/components/data/menuData.js
@@ -0,0 +1,12 @@
+export const mainMenu = [
+     {link:"#", content:"Listen", icon:"lucide:home"},
+     {link:"#about", content:"Events", icon:"octicon:person-24"},
+     {link:"#portafolio", content:"Video", icon:"carbon:portfolio"},
+     {link:"#blog", content:"Gallery", icon:"mdi:blog"},
+     {link:"#blog", content:"Contact", icon:"mdi:blog"}
+]
+export const menuBlog = [
+     {link:"/", content:"Home", icon:"lucide:home"},
+     {link:"/#about", content:"About", icon:"octicon:person-24"},
+     {link:"/#portafolio", content:"Portfolio", icon:"carbon:portfolio"},
+]
